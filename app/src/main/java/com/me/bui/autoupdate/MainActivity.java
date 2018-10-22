c95b31533e535afe87f51a1a01cd3df8e5d8ac3e
maobui <mao.buidinh@hotmail.com>
2018-10-22 17:35:51 +0700

    - test install apk.

--- app/src/main/java/com/me/bui/autoupdate/MainActivity.java
+++ app/src/main/java/com/me/bui/autoupdate/MainActivity.java
@@ -1,13 +1,68 @@
 package com.me.bui.autoupdate;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.util.List;
+
+import static android.content.Intent.ACTION_INSTALL_PACKAGE;
 
 public class MainActivity extends AppCompatActivity {
+    private final String APK_URL = "https://github.com/EmpireCTF/empirectf/blob/master/writeups/2018-06-19-SCTF/files/simple.apk";
 
+    Button btnInstallApk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder(); StrictMode.setVmPolicy(builder.build());
+        initView();
+
+//        Intent updateIntent = new Intent(Intent.ACTION_VIEW,
+//                Uri.parse("https://github.com/EmpireCTF/empirectf/blob/master/writeups/2018-06-19-SCTF/files/simple.apk"));
+//        startActivity(updateIntent);
+    }
+
+    private void initView() {
+        btnInstallApk = findViewById(R.id.btnInstallApk);
+        btnInstallApk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                installApkFile("HardwareEventLMU_release_v1.1.2_181022.apk");
+            }
+        });
+    }
+
+    private void installApkFile(String fileApk) {
+        File newApk = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/"  + fileApk);
+//        newApk.setReadable(true, false);
+        Intent intent = new Intent();
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            uri = FileProvider.getUriForFile(this,BuildConfig.APPLICATION_ID + ".provider",  newApk);
+            intent.setAction(Intent.ACTION_INSTALL_PACKAGE);
+//            intent.setAction(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true);
+        } else {
+            uri = Uri.fromFile(newApk);
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        }
+        intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        startActivity(intent);
+        this.finish();
     }
 }
---
+++ app/src/main/java/com/me/bui/autoupdate/Utils.java
@@ -0,0 +1,95 @@
+package com.me.bui.autoupdate;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Utils {
+    public Utils() { }
+
+    public static int getVersionCode(Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public static String getVersionName(Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String downloadText() {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = openHttpConnection();
+        } catch (IOException e1) {
+            return "";
+        }
+
+        String str = "";
+        if (in != null) {
+            InputStreamReader isr = new InputStreamReader(in);
+            int charRead;
+            char[] inputBuffer = new char[BUFFER_SIZE];
+            try {
+                while ((charRead = isr.read(inputBuffer)) > 0) {
+                    // ---convert the chars to a String---
+                    String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                    str += readString;
+                    inputBuffer = new char[BUFFER_SIZE];
+                }
+                in.close();
+            } catch (IOException e) {
+                return "";
+            }
+        }
+        return str;
+    }
+
+    private InputStream openHttpConnection() throws IOException {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL("");//new URL(getVersionFile());
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+}
