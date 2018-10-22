8b2025e8181750712564c3b31f0cd6be798d2098
mao.buidinh <mao.buidinh@hotmail.com>
2018-10-22 23:07:08 +0700

    - 1. use AsyncTask to download file.

--- app/src/main/java/com/me/bui/autoupdate/MainActivity.java
+++ app/src/main/java/com/me/bui/autoupdate/MainActivity.java
@@ -1,11 +1,16 @@
 package com.me.bui.autoupdate;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
+import android.os.PowerManager;
 import android.os.StrictMode;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -15,14 +20,22 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import static android.content.Intent.ACTION_INSTALL_PACKAGE;
 
 public class MainActivity extends AppCompatActivity {
-    private final String APK_URL = "https://github.com/maobui/new_version/blob/master/apk/sample.apk";
+    private final String APK_URL = "https://github.com/maobui/new_version/raw/master/apk/sample.apk";
+    private final String APK_NAME = "sample.apk";
 
     Button btnInstallApk;
+    Button btnDownload;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,18 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
 //        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder(); StrictMode.setVmPolicy(builder.build());
         initView();
-
-//        Intent updateIntent = new Intent(Intent.ACTION_VIEW,
-//                Uri.parse("https://github.com/EmpireCTF/empirectf/blob/master/writeups/2018-06-19-SCTF/files/simple.apk"));
-//        startActivity(updateIntent);
     }
 
     private void initView() {
         btnInstallApk = findViewById(R.id.btnInstallApk);
+        btnDownload = findViewById(R.id.btnDownload);
+
         btnInstallApk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                installApkFile("sample.apk");
+                installApkFile(APK_NAME);
+            }
+        });
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dowloadApk(APK_URL);
             }
         });
     }
@@ -69,5 +87,129 @@ public class MainActivity extends AppCompatActivity {
         intent.setDataAndType(uri, "application/vnd.android.package-archive");
         startActivity(intent);
         this.finish();
+    }
+
+    // declare the dialog as a member field of your activity
+    ProgressDialog mProgressDialog;
+
+    private void dowloadApk(String apk_url) {
+
+        // instantiate it within the onCreate method
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setMessage("A message");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setCancelable(true);
+
+        // execute this when the downloader must be fired
+        final DownloadTask downloadTask = new DownloadTask(MainActivity.this);
+        downloadTask.execute(apk_url);
+
+        mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                downloadTask.cancel(true);
+            }
+        });
+    }
+
+    // usually, subclasses of AsyncTask are declared inside the activity class.
+    // that way, you can easily modify the UI thread from here
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+
+        public DownloadTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+
+                // download the file
+                input = connection.getInputStream();
+                output = new FileOutputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + APK_NAME);
+
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if (isCancelled()) {
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    // publishing the progress....
+                    if (fileLength > 0) // only if total length is known
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // take CPU lock to prevent CPU from going off if the user
+            // presses the power button during download
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            mWakeLock.acquire();
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            // if we get here, length is known, now set indeterminate to false
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(progress[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mWakeLock.release();
+            mProgressDialog.dismiss();
+            if (result != null)
+                Toast.makeText(context,"Download error: "+result, Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(context,"File downloaded", Toast.LENGTH_SHORT).show();
+        }
     }
 }
